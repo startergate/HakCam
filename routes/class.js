9ec3904f6c99be8d5fa790e8f13a369b36dc72ed
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-08-07 07:13:27 +0900

    ertfygh

--- routes/class.js
+++ routes/class.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:class', function(req, res, next) {
-    res.render('class', {class: req.params.class});
+router.get('/:class', (req, res, next) => {
+    res.render('class', {classes: req.params.class});
 
 });
+
+router.get('/:class/:lecture', (req, res, next) => {
+    res.render('stream', {classes: req.params.class, lecture: req.params.lecture});
+})
 
 module.exports = router;
